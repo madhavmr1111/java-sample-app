6a913888606162bed0affac182a3e94ed678f4ab
Mani <msivanes@redhat.com>
2016-02-20 18:35:07 -0500

    Initial commit.

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,24 @@
+
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author msivanes
+ */
+public class MainTest {
+
+    public MainTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        assert true;
+    }
+
+}
